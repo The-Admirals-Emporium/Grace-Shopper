c975b42656bc0912cad22fa4bb8f562511946984
Natasha Whitney <natiwhitney@gmail.com>
2020-03-03 11:54:31 -0500

    Model Relationships test file, test user -> orders

---
+++ server/db/models/model-relationships.spec.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const db = require('../index');
+const { User, Boat, Order } = require('../models');
+
+describe('User >-< Order Association', () => {
+  before(() => db.sync({ force: true }));
+  afterEach(() => db.sync({ force: true }));
+
+  describe('Sequelize Models', () => {
+    it('a user may belong to many orders', async () => {
+      const user = await User.create({ email: 'natiwhitney@gmail.com' });
+      const orderInProgress = await Order.create({ status: 'PENDING' });
+      const orderCompleted = await Order.create({ status: 'COMPLETED' });
+      await user.addOrders([orderInProgress, orderCompleted]);
+      const userOrders = await user.getOrders().map(order => order.status);
+      expect(userOrders).to.deep.equal(['PENDING', 'COMPLETED']);
+    });
+  });
+});
+
+//     it('a robot may belong to many projects', async () => {
+//       const openPodBayDoors = await Project.create({ title: 'Open the pod bay doors' })
+//       const makePizza = await Project.create({ title: 'Make pizza' })
+//       const hal9000 = await Robot.create({ name: 'HAL-9000' })
+//       await hal9000.addProjects([openPodBayDoors, makePizza])
+//       const hal9000sProjects = await hal9000.getProjects().map(title => title.title)
+//       expect(hal9000sProjects).to.deep.equal(['Open the pod bay doors', 'Make pizza'])
+//     })
+
+//   })
