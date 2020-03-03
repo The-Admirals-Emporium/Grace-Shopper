b2676338a92070f954e8821af050946c40a8fe4b
Natasha Whitney <natiwhitney@gmail.com>
2020-03-03 13:48:19 -0500

    Use Bluebird Promise & seed users with orders

--- script/seed.js
+++ script/seed.js
@@ -1,18 +1,47 @@
 'use strict';
+var Promise = require('bluebird');
 
 const db = require('../server/db');
-const { User } = require('../server/db/models');
+const { User, Order } = require('../server/db/models');
+
+var ordersData = [
+  { status: 'COMPLETED' },
+  { status: 'PENDING' },
+  { status: 'REFUNDED' },
+];
+
+var usersData = [
+  { email: 'cody@email.com', password: '123' },
+  { email: 'murphy@email.com', password: '123' },
+];
+
+var usersDataWithOrders = [
+  { email: 'cody@aol.com', password: '123' },
+  { email: 'murphy@aol.com', password: '123' },
+];
 
 async function seed() {
   await db.sync({ force: true });
   console.log('db synced!');
 
-  const users = await Promise.all([
-    User.create({ email: 'cody@email.com', password: '123' }),
-    User.create({ email: 'murphy@email.com', password: '123' }),
-  ]);
+  const users = await Promise.map(usersData, function(user) {
+    return db.model('user').create(user);
+  });
 
-  console.log(`seeded ${users.length} users`);
+  console.log(`seeded ${users.length} users with no orders`);
+
+  const usersWithOrders = await Promise.map(usersDataWithOrders, function(
+    user
+  ) {
+    return db
+      .model('user')
+      .create(Object.assign(user, { orders: ordersData }), {
+        include: [Order],
+      });
+  });
+
+  console.log(`seeded ${usersWithOrders.length} users with multiple orders`);
+
   console.log(`seeded successfully`);
 }
 
