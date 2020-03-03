0343288a9ac61a36d29d4cc3cae663682f672a6b
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-03 15:15:51 -0500

    added boat route (TODO) to index

--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 module.exports = router;
 
 router.use('/users', require('./users'));
+router.use('/boats', require('./boats'));
 
 router.use((req, res, next) => {
   const error = new Error('Not Found');
