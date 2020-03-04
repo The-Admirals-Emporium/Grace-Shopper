f8f02d495ef828b20b0574ee6d172c0fe1f4b548
JoshuaSkootsky <joshua.skootsky@gmail.com>
2020-03-04 12:43:30 -0500

    Closes #59 - remove unused import of useEffect hook

--- client/components/boatList.js
+++ client/components/boatList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getAllBoats } from '../store';
 import { Link } from 'react-router-dom';
