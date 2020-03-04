f360de56276d6fe6c926e7df6feeea71f9bf5d9a
Amantina Sosa <amantinasosa@gmail.com>
2020-03-04 17:37:22 -0500

    one test spec fixed

--- server/api/users.js
+++ server/api/users.js
@@ -4,18 +4,17 @@ module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
+    //if not logged in or not admin cannot see users
+    if (!req.users || !req.isAdmin) {
+      return res.send('[]').status(403);
+    }
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
       attributes: ['id', 'email'],
     });
-    //if not logged in or not admin cannot see users
-    if (!req.users || !req.isAdmin) {
-      return res.send(403);
-    } else {
-      res.json(users);
-    }
+    res.json(users);
   } catch (err) {
     next(err);
   }
