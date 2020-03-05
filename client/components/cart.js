4680f655c57dbcda91f91ffc238fb6a79e45ced8
Natasha Whitney <natiwhitney@gmail.com>
2020-03-05 13:47:04 -0500

    Render boats in cart

--- client/components/cart.js
+++ client/components/cart.js
@@ -9,6 +9,25 @@ const Cart = props => {
       <div>
         <h3>Status: {cart.status}</h3>
         <h3>Total: $ USD {cart.total}</h3>
+        <ul>
+          {cart.boats.map(boat => {
+            return (
+              <li key={boat.id}>
+                <p>Name: {boat.name}</p>
+                <p>Cost: {boat.cost}</p>
+                <p>Quantity: to-do</p>
+                <button
+                  type="button"
+                  disabled={!boat.inventory}
+                  size="small"
+                  color="primary"
+                >
+                  Remove
+                </button>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     );
   } else {
