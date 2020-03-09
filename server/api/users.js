bb98ce93cef826ab4dab20ff6f3075e803ca479b
Natasha Whitney <natiwhitney@gmail.com>
2020-03-08 23:30:59 -0400

    Create route to load user order from database

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const { User } = require('../db/models');
-const { isAdmin } = require('./gateway.js');
+const { User, Order, Boat } = require('../db/models');
+const { isAdmin, isCorrectUser } = require('./gateway.js');
 
 module.exports = router;
 
@@ -13,6 +13,35 @@ router.get('/', isAdmin, async (req, res, next) => {
       attributes: ['id', 'email'],
     });
     res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// TKTK move this to orders route
+router.get('/:id', isCorrectUser, async (req, res, next) => {
+  try {
+    // TKTK we could possibly use findOrCreate here
+    // TKTK calculate total
+    // TKTK through-table just send back quantity
+
+    // find an order that has boats
+    const order = await Order.findOne({
+      where: {
+        userId: +req.params.id,
+        status: 'PENDING',
+      },
+      attributes: ['id', 'status', 'shippingAddress', 'total'],
+      include: [
+        { model: Boat, attributes: ['id', 'cost', 'name', 'imageUrl'] },
+      ],
+    });
+
+    const cart = order.dataValues;
+
+    //cart.calculateTotal()
+
+    res.json(cart);
   } catch (err) {
     next(err);
   }
