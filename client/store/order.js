c1d9caf37bf74f97d32d5a677d074d16447a9545
Natasha Whitney <natiwhitney@gmail.com>
2020-03-08 23:33:37 -0400

    Add logic to store to get user cart from database

--- client/store/order.js
+++ client/store/order.js
@@ -5,6 +5,7 @@ import history from '../history';
  * ACTION TYPES
  */
 const GET_CART = 'GET_CART';
+const GET_USER_CART = 'GET_USER_CART';
 const UPDATE_CART = 'UPDATE_CART';
 /**
  * INITIAL STATE
@@ -15,33 +16,31 @@ const defaultCart = { status: 'PENDING', boats: [] };
  * ACTION CREATORS
  */
 const getCart = cart => ({ type: GET_CART, cart });
+const getUserCart = cart => ({ type: GET_USER_CART, cart });
 export const updateCart = boat => ({ type: UPDATE_CART, boat });
 
 /**
  * THUNK CREATORS
  */
-export const cart = user => async dispatch => {
+export const guestCart = () => async dispatch => {
   let cart;
 
   try {
-    // switch to session storage
     if (window.localStorage.getItem('cart')) {
       console.log(
         'grabbing cart from window local storage',
         window.localStorage.getItem('cart')
       );
       cart = JSON.parse(window.localStorage.getItem('cart'));
-    } else if (!user.length) {
+    } else {
       // guest
 
-      let res = await axios.post('/api/orders'); // object
+      let res = await axios.post('/api/orders'); // creates a new cart
 
       cart = res.data;
 
       console.log('getting new cart template from database', cart);
       window.localStorage.setItem('cart', JSON.stringify(cart));
-    } else {
-      // user, load from database
     }
     dispatch(getCart(cart));
   } catch (err) {
