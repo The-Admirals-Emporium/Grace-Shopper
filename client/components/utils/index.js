98e9397b96512ad74d7239215f416d762f2b6ffe
Natasha Whitney <natiwhitney@gmail.com>
2020-03-10 11:42:25 -0400

    Fix costDisplay util -> display 0 correctly

--- client/components/utils/index.js
+++ client/components/utils/index.js
@@ -1,3 +1,4 @@
 export const costDisplay = (costInt, display = '$ USD ') => {
-  return display + (costInt / 100).toFixed(2);
+  const formattedCost = costInt ? (costInt / 100).toFixed(2) : 0;
+  return display + formattedCost;
 };
